dabdca65c6d4ee7ab682a581d1e3bb497d252ef2
Yankes <Yankes@users.noreply.github.com>
2015-04-19 15:14:20 +0200

    Script generating readme

---
+++ createDocExtended.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+commitID=$(git rev-parse HEAD)
+sed \
+  -e 's/\[b\]//g' \
+  -e 's/\[\/b\]//g' \
+  -e 's/\[i\]//g' \
+  -e 's/\[\/i\]//g' \
+  -e 's/\[br\/\]//g' \
+  -e 's/\[commit\/\]/'$commitID'/g' \
+  ../Extended.txt > Readme.txt
+sed \
+  -e 's/  /\&nbsp;\&nbsp;/g' \
+  -e 's/ #/\&nbsp;#/g' \
+  -e 's/\[b\]/<h2 style="font-style:italic"><strong>/g' \
+  -e 's/\[\/b\]/<\/strong><\/h2>/g' \
+  -e 's/\[i\]/<p><strong>/g' \
+  -e 's/\[\/i\]/<\/strong><\/p>/g' \
+  -e 's/\[code\]/<div style="font-family: monospace; white-space: pre;">/g' \
+  -e 's/\[\/code\]/<\/div>/g' \
+  -e 's/\[br\/\]/<br \/>/g' \
+  -e 's/\[commit\/\]/'$commitID'/g' \
+  ../Extended.txt > OpenXcomExWeb.txt
+
+sed \
+  -e 's/\[b\]/\[b\]\[size=14pt\]/g' \
+  -e 's/\[\/b\]/\[\/size\]\[\/b\]/g' \
+  -e 's/\[br\/\]//g' \
+  -e 's/\[i\]/\[b\]/g' \
+  -e 's/\[\/i\]/\[\/b\]/g' \
+  -e 's/\[commit\/\]/'$commitID'/g' \
+  ../Extended.txt > OpenXcomExForum.txt
