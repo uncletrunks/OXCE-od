950486763b4cedb69445fc30fa8a60e5a36ffa50
Meridian <meridian@centrum.sk>
2016-01-04 22:39:44 +0100

    Updated version info
    Signed-off-by: Meridian <meridian@centrum.sk>

--- src/version.h
+++ src/version.h
@@ -24,7 +24,7 @@
 #define OPENXCOM_VERSION_NUMBER 1,0,0,0
 
 #ifndef OPENXCOM_VERSION_GIT
-#define OPENXCOM_VERSION_GIT " (v2016-01-03)"
+#define OPENXCOM_VERSION_GIT " (v2016-01-04)"
 #endif
 
 #endif
