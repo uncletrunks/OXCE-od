e79af91570b02d010941663432e4c9bdf4c9ea02
Yankes <Yankes@users.noreply.github.com>
2016-01-15 00:42:54 +0100

    Update readme

--- createDocExtended.sh
+++ createDocExtended.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 commitID=$(git rev-parse HEAD)
+commitDate=$(date -d"$(git show -s --format=%ci $commitID)" +%Y-%m-%d)
 openXcomID=$(git merge-base HEAD supsuper/master)
 openXcomDate=$(date -d"$(git show -s --format=%ci $openXcomID)" +%Y-%m-%d)
 
@@ -10,7 +11,8 @@ sed \
   -e 's/\[\/i\]//g' \
   -e 's/\[br\/\]//g' \
   -e 's/\[commit\/\]/'$commitID'/g' \
-  -e 's/\[commitOriginDate\/\]/'$openXcomDate'/g' \
+  -e 's/\[date\/\]/'$commitDate'/g' \
+  -e 's/\[dateOrigin\/\]/'$openXcomDate'/g' \
   -e 's/\[commitOrigin\/\]/'$openXcomID'/g' \
   -e 's/\[examples\]//g' \
   -e 's/\[\/examples\]//g' \
@@ -26,7 +28,8 @@ sed \
   -e 's/\[\/code\]/<\/div>/g' \
   -e 's/\[br\/\]/<br \/>/g' \
   -e 's/\[commit\/\]/'$commitID'/g' \
-  -e 's/\[commitOriginDate\/\]/'$openXcomDate'/g' \
+  -e 's/\[date\/\]/'$commitDate'/g' \
+  -e 's/\[dateOrigin\/\]/'$openXcomDate'/g' \
   -e 's/\[commitOrigin\/\]/'$openXcomID'/g' \
   -e 's/\[examples\]//g' \
   -e 's/\[\/examples\]//g' \
@@ -39,7 +42,8 @@ sed \
   -e 's/\[i\]/\[b\]/g' \
   -e 's/\[\/i\]/\[\/b\]/g' \
   -e 's/\[commit\/\]/'$commitID'/g' \
-  -e 's/\[commitOriginDate\/\]/'$openXcomDate'/g' \
+  -e 's/\[date\/\]/'$commitDate'/g' \
+  -e 's/\[dateOrigin\/\]/'$openXcomDate'/g' \
   -e 's/\[commitOrigin\/\]/'$openXcomID'/g' \
   -e '/\[examples\]/{$!{:re;N;s/\[\/examples\]//;ty;bre;:y;d;}}' \
   ../Extended.txt > OpenXcomExForum.txt
