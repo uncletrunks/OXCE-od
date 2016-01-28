ab560b1d66d21eb944c9153ea2f1e3d4af3bb90a
Meridian <meridian@centrum.sk>
2016-01-28 17:58:32 +0100

    Updated version info

--- src/version.h
+++ src/version.h
@@ -24,7 +24,7 @@
 #define OPENXCOM_VERSION_NUMBER 1,0,0,0
 
 #ifndef OPENXCOM_VERSION_GIT
-#define OPENXCOM_VERSION_GIT " (v2016-01-24)"
+#define OPENXCOM_VERSION_GIT " (v2016-01-28)"
 #endif
 
 #endif
