66842b6a6fe8ba9da3f2ccbae4a868d717bfb24a
Meridian <meridian@centrum.sk>
2016-01-28 17:58:19 +0100

    Added support for day/night indicator

--- src/Geoscape/ConfirmLandingState.cpp
+++ src/Geoscape/ConfirmLandingState.cpp
@@ -23,6 +23,7 @@
 #include "../Interface/Window.h"
 #include "../Interface/Text.h"
 #include "../Interface/TextButton.h"
+#include "../Engine/SurfaceSet.h"
 #include "../Savegame/SavedBattleGame.h"
 #include "../Savegame/SavedGame.h"
 #include "../Savegame/Craft.h"
@@ -59,6 +60,7 @@ ConfirmLandingState::ConfirmLandingState(Craft *craft, Texture *texture, int sha
 	_btnNo = new TextButton(80, 20, 136, 150);
 	_txtMessage = new Text(206, 80, 25, 40);
 	_txtBegin = new Text(206, 17, 25, 130);
+	_sprite = new Surface(24, 24, 202, 30);
 
 	// Set palette
 	setInterface("confirmLanding");
@@ -68,6 +70,7 @@ ConfirmLandingState::ConfirmLandingState(Craft *craft, Texture *texture, int sha
 	add(_btnNo, "button", "confirmLanding");
 	add(_txtMessage, "text", "confirmLanding");
 	add(_txtBegin, "text", "confirmLanding");
+	add(_sprite);
 
 	centerAllSurfaces();
 
@@ -94,6 +97,26 @@ ConfirmLandingState::ConfirmLandingState(Craft *craft, Texture *texture, int sha
 	std::wostringstream ss;
 	ss << L'\x01' << tr("STR_BEGIN_MISSION");
 	_txtBegin->setText(ss.str().c_str());
+
+	SurfaceSet *sprites = _game->getMod()->getSurfaceSet("DayNightIndicator");
+	if (sprites != 0)
+	{
+		if (_shade <= 0)
+		{
+			// day (0)
+			sprites->getFrame(0)->blit(_sprite);
+		}
+		else if (_shade > _game->getMod()->getMaxDarknessToSeeUnits())
+		{
+			// night (9-15); note: 9 is configurable in the ruleset in OXCE
+			sprites->getFrame(1)->blit(_sprite);
+		}
+		else
+		{
+			// dusk/dawn (1-8)
+			sprites->getFrame(2)->blit(_sprite);
+		}
+	}
 }
 
 /**
