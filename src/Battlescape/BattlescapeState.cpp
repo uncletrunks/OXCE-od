396f4dc5ea8d95251fe73aa3a07800e4b4c9a7d6
Meridian <meridian@centrum.sk>
2016-03-09 21:19:51 +0000

    Added fatal wounds infobox

--- src/Battlescape/BattlescapeState.cpp
+++ src/Battlescape/BattlescapeState.cpp
@@ -1803,6 +1803,22 @@ inline void BattlescapeState::handle(Action *action)
 				{
 					_game->pushState(new InfoboxState(_save->hitLog.str()));
 				}
+				// "ctrl-w" - show fatal wounds
+				else if (action->getDetails()->key.keysym.sym == SDLK_w && (SDL_GetModState() & KMOD_CTRL) != 0)
+				{
+					int wounds = 0;
+					for (std::vector<BattleUnit*>::iterator i = _save->getUnits()->begin(); i != _save->getUnits()->end(); ++i)
+					{
+						if ((*i)->getOriginalFaction() == FACTION_PLAYER && (*i)->getHealth() > 0 && (*i)->getFatalWounds() > 0)
+						{
+							wounds++;
+						}
+					}
+					std::wostringstream ss;
+					ss << L"Units with fatal wounds: ";
+					ss << wounds;
+					_game->pushState(new InfoboxState(ss.str()));
+				}
 				if (Options::debug)
 				{
 					// "ctrl-d" - enable debug mode
