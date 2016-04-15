505d47d0d7ace5219fd137a7349fcabd84cd61f1
Meridian <meridian@centrum.sk>
2016-04-15 21:13:43 +0200

    Properly display name of completed research topic
    - in case you researched something multiple times (e.g. Sectoid Medic)
    the game didn't tell you other than first time

--- src/Geoscape/ResearchCompleteState.h
+++ src/Geoscape/ResearchCompleteState.h
@@ -41,7 +41,7 @@ class ResearchCompleteState : public State
 	const RuleResearch * _research, * _bonus;
 public:
 	/// Creates the EndResearch state.
-	ResearchCompleteState(const RuleResearch * research, const RuleResearch * bonus);
+	ResearchCompleteState(const RuleResearch *newResearch, const RuleResearch *bonus, const RuleResearch *research);
 	/// Handler for clicking the OK button.
 	void btnOkClick(Action *action);
 	/// Handler for clicking the Report button.
