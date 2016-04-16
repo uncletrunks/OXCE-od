094e0da79fa145bdc774b83bab4ba80d830ef329
Meridian <meridian@centrum.sk>
2016-04-16 19:52:16 +0200

    Revert "Hardcoded maxViewDistance to 20 again"
    This reverts commit 295cd465ddbe81917d4f35a3a0a31d6f43cbfe6f.

--- src/Mod/Mod.cpp
+++ src/Mod/Mod.cpp
@@ -1052,8 +1052,7 @@ void Mod::loadFile(const std::string &filename)
 	{
 		_startingTime.load(doc["startingTime"]);
 	}
-	// sorry, 20 is really the most we can do without affecting performance :(
-	//_maxViewDistance = doc["maxViewDistance"].as<int>(_maxViewDistance);
+	_maxViewDistance = doc["maxViewDistance"].as<int>(_maxViewDistance);
 	_maxDarknessToSeeUnits = doc["maxDarknessToSeeUnits"].as<int>(_maxDarknessToSeeUnits);
 	_costEngineer = doc["costEngineer"].as<int>(_costEngineer);
 	_costScientist = doc["costScientist"].as<int>(_costScientist);
