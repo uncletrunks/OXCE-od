4932967a32b7d7a0f282d99cc105b8b999905c83
Meridian <meridian@centrum.sk>
2016-06-17 22:32:13 +0200

    Added craftName attribute to addCraft map script command

--- src/Mod/MapScript.h
+++ src/Mod/MapScript.h
@@ -61,7 +61,7 @@ private:
 	int _sizeX, _sizeY, _sizeZ, _executionChances, _executions, _cumulativeFrequency, _label;
 	MapDirection _direction;
 	TunnelData *_tunnelData;
-	std::string _ufoName;
+	std::string _ufoName, _craftName;
 
 	/// Randomly generate a group from within the array.
 	int getGroupNumber();
@@ -104,6 +104,8 @@ public:
 	MapBlock *getNextBlock(RuleTerrain *terrain);
 	/// Gets the UFO's name (for setUFO)
 	std::string getUFOName();
+	/// Gets the craft's name (for addCraft)
+	std::string getCraftName();
 };
 }
 #endif
