6b0ee2ccae17725613ec2b626bbd1d813777f599
Meridian <meridian@centrum.sk>
2016-10-06 19:56:29 +0200

    Heat vision and psi vision (reintroduced)

--- src/Battlescape/TileEngine.cpp
+++ src/Battlescape/TileEngine.cpp
@@ -967,11 +967,22 @@ bool TileEngine::visible(BattleUnit *currentUnit, Tile *tile)
 	if (currentUnit->getFaction() == tile->getUnit()->getFaction()) return true;
 
 	// if beyond global max. range, nobody can see anyone
-	if (distanceSq(currentUnit->getPosition(), tile->getPosition(), false) > getMaxViewDistanceSq())
+	int currentDistanceSq = distanceSq(currentUnit->getPosition(), tile->getPosition(), false);
+	if (currentDistanceSq > getMaxViewDistanceSq())
 	{
 		return false;
 	}
 
+	// psi vision
+	int psiVisionDistance = currentUnit->getArmor()->getPsiVision();
+	bool fearImmune = tile->getUnit()->getArmor()->getFearImmune();
+	if (psiVisionDistance > 0 && !fearImmune)
+	{
+		if (currentDistanceSq <= (psiVisionDistance * psiVisionDistance))
+		{
+			return true; // we already sense the unit, no need to check obstacles or smoke
+		}
+	}
 
 	int visibleDistanceMaxVoxel = getMaxVoxelViewDistance();
 	// during dark aliens can see 20 tiles, xcom can see 9 by default... unless overridden by armor
@@ -990,6 +1001,9 @@ bool TileEngine::visible(BattleUnit *currentUnit, Tile *tile)
 	Position scanVoxel;
 	std::vector<Position> _trajectory;
 	bool unitSeen = canTargetUnit(&originVoxel, tile, &scanVoxel, currentUnit);
+
+	// heat vision 100% = smoke effectiveness 0%
+	int smokeDensityFactor = 100 - currentUnit->getArmor()->getHeatVision();
 
 	if (unitSeen)
 	{
@@ -1024,9 +1038,9 @@ bool TileEngine::visible(BattleUnit *currentUnit, Tile *tile)
 				densityOfFire += t->getFire();
 			}
 		}
-		auto visibilityQuality = visibleDistanceMaxVoxel - visibleDistanceVoxels - densityOfSmoke * getMaxViewDistance()/(3 * 20);
+		auto visibilityQuality = visibleDistanceMaxVoxel - visibleDistanceVoxels - densityOfSmoke * smokeDensityFactor * getMaxViewDistance()/(3 * 20 * 100);
 		ModScript::VisibilityUnitParser::Output arg{ visibilityQuality, visibilityQuality, ScriptTag<BattleUnitVisibility>::getNullTag() };
-		ModScript::VisibilityUnitParser::Worker worker{ currentUnit, tile->getUnit(), visibleDistanceVoxels, visibleDistanceMaxVoxel, densityOfSmoke, densityOfFire };
+		ModScript::VisibilityUnitParser::Worker worker{ currentUnit, tile->getUnit(), visibleDistanceVoxels, visibleDistanceMaxVoxel, densityOfSmoke * smokeDensityFactor / 100, densityOfFire };
 		worker.execute(currentUnit->getArmor()->getVisibilityUnitScript(), arg);
 		unitSeen = 0 < arg.getFirst();
 	}
