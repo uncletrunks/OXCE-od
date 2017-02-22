abca713f16dbc3446176ebaff76fbd6d5798b27c
Meridian <meridian@centrum.sk>
2017-02-22 22:00:36 +0100

    Unified (and fixed) chart labeling algorithm
    Country lines were drawn incorrectly.
    When there was negative score, the *full* range was compared against only 8/9 of available chart space.
    
    Technical:
    1. the "check" is calculated against (0, range)
    2. but "range" includes the portion below X axis too, so grids-- is not necessary/is wrong
    3. the shift below X axis is done later (correctly)

--- src/Geoscape/GraphsState.cpp
+++ src/Geoscape/GraphsState.cpp
@@ -721,7 +721,6 @@ void GraphsState::drawCountryLines()
 	double range = upperLimit - lowerLimit;
 	double low = lowerLimit;
 	int grids = 9; // cells in grid
-	if (low<0) grids--;
 	int check = _income ? 50 : 10;
 	while (range > check * grids)
 	{
@@ -902,14 +901,13 @@ void GraphsState::drawRegionLines()
 	double low = lowerLimit;
 	int check = 10;
 	int grids = 9; // cells in grid
-	if (low<0) grids--;
 	while (range > check * grids)
 	{
 		check *= 2;
 	}
 
 	lowerLimit = 0;
-	upperLimit = check * 9;
+	upperLimit = check * grids;
 
 	if (low < 0)
 	{
@@ -1096,14 +1094,13 @@ void GraphsState::drawFinanceLines()
 	double low = lowerLimit;
 	int check = 250;
 	int grids = 9; // cells in grid
-	if (low<0) grids--;
 	while (range > check * grids)
 	{
 		check *= 2;
 	}
 
 	lowerLimit = 0;
-	upperLimit = check * 9;
+	upperLimit = check * grids;
 
 	if (low < 0)
 	{
