b8b27d9670c5b00b0db9f619426e5ed427cf94b8
Yankes <Yankes@users.noreply.github.com>
2017-06-08 23:10:29 +0200

    Version bump to 3.8a

--- src/version.h
+++ src/version.h
@@ -18,7 +18,7 @@
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define OPENXCOM_VERSION_SHORT "3.8"
+#define OPENXCOM_VERSION_SHORT "3.8a"
 #define OPENXCOM_VERSION_LONG "3.8.0.0"
 #define OPENXCOM_VERSION_NUMBER 3,8,0,0
 
