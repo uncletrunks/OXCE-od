41e2da1bde43b8c0a3c837e6a02470114a8dde09
Yankes <Yankes@users.noreply.github.com>
2017-08-12 18:06:36 +0200

    Version bump to 3.9c

--- src/version.h
+++ src/version.h
@@ -18,7 +18,7 @@
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define OPENXCOM_VERSION_SHORT "3.9b"
+#define OPENXCOM_VERSION_SHORT "3.9c"
 #define OPENXCOM_VERSION_LONG "3.9.0.0"
 #define OPENXCOM_VERSION_NUMBER 3,9,0,0
 
