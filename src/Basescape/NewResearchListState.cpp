5fb565cd9d7b31978b4a152a38b55cd389b1ffb0
Yankes <Yankes@users.noreply.github.com>
2018-09-09 15:28:40 +0200

    Fix reserch project order after refactor

--- src/Basescape/NewResearchListState.cpp
+++ src/Basescape/NewResearchListState.cpp
@@ -110,6 +110,8 @@ void NewResearchListState::fillProjectList()
 	_lstResearch->clearList();
 	// Note: this is the *only* place where this method is called with considerDebugMode = true
 	_game->getSavedGame()->getAvailableResearchProjects(_projects, _game->getMod() , _base, true);
+	// sort by list order
+	std::sort(_projects.begin(), _projects.end(), [&](RuleResearch* a, RuleResearch* b) { return a->getListOrder() < b->getListOrder(); });
 	std::vector<RuleResearch*>::iterator it = _projects.begin();
 	while (it != _projects.end())
 	{
