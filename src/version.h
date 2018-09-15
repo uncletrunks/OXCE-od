f2005ae8a8cad6df07a48595919a74face0405bc
Meridian <meridian@centrum.sk>
2018-09-15 07:41:13 +0200

    Updated version info and dropped the +

--- src/version.h
+++ src/version.h
@@ -18,7 +18,7 @@
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define OPENXCOM_VERSION_SHORT "Extended+ 4.0"
+#define OPENXCOM_VERSION_SHORT "Extended 4.0"
 #define OPENXCOM_VERSION_LONG "4.0.0.0"
 #define OPENXCOM_VERSION_NUMBER 4,0,0,0
 
@@ -27,5 +27,5 @@
 #endif
 
 #ifndef OPENXCOM_VERSION_GIT
-#define OPENXCOM_VERSION_GIT " (v2018-09-07)"
+#define OPENXCOM_VERSION_GIT " (v2018-09-15)"
 #endif
