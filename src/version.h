f3f8f41d97dde72771df6b67d828e33e19a8cadb
Yankes <Yankes@users.noreply.github.com>
2018-09-19 23:09:46 +0200

    Version bump to 5.0

--- src/version.h
+++ src/version.h
@@ -18,9 +18,9 @@
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define OPENXCOM_VERSION_SHORT "4.0a"
-#define OPENXCOM_VERSION_LONG "4.0.0.0"
-#define OPENXCOM_VERSION_NUMBER 4,0,0,0
+#define OPENXCOM_VERSION_SHORT "5.0"
+#define OPENXCOM_VERSION_LONG "5.0.0.0"
+#define OPENXCOM_VERSION_NUMBER 5,0,0,0
 
 #ifdef GIT_BUILD
 #include "git_version.h"
