b34184f89df8f900b140d6ea5552538e9a4e1cee
Stoddard <stoddard@lxnt.info>
2019-02-12 21:16:19 +0300

    pathFromWindows(): get rid of lowercasing

--- src/Engine/CrossPlatform.cpp
+++ src/Engine/CrossPlatform.cpp
@@ -120,14 +120,10 @@ void getErrorDialog()
  */
 static std::string pathFromWindows(const wchar_t *pathW) {
 	int sizeW = lstrlenW(pathW);
-	wchar_t *pathWLC = (wchar_t *)malloc(sizeof(wchar_t) * (sizeW + 1));
-	memcpy(pathWLC, pathW, sizeof(wchar_t) * (sizeW + 1));
-	CharLowerW(pathWLC);
-	int sizeU8 = WideCharToMultiByte(CP_UTF8, 0, pathWLC, sizeW, NULL, 0, NULL, NULL);
+	int sizeU8 = WideCharToMultiByte(CP_UTF8, 0, pathW, sizeW, NULL, 0, NULL, NULL);
 	std::string pathU8(sizeU8, 0);
-	WideCharToMultiByte(CP_UTF8, 0, pathWLC, sizeW, &pathU8[0], sizeU8, NULL, NULL);
+	WideCharToMultiByte(CP_UTF8, 0, pathW, sizeW, &pathU8[0], sizeU8, NULL, NULL);
 	std::replace(pathU8.begin(), pathU8.end(), '\\', '/');
-	free(pathWLC);
 	return pathU8;
 }
 
