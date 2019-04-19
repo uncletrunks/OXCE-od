28a58f130d934290f70da4da51060249a3ec46f1
Meridian <meridian@centrum.sk>
2019-04-19 10:34:10 +0200

    Updated version info

--- src/version.h
+++ src/version.h
@@ -18,10 +18,10 @@
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define OPENXCOM_VERSION_SHORT "Extended 5.3.3"
-#define OPENXCOM_VERSION_LONG "5.3.3.0"
-#define OPENXCOM_VERSION_NUMBER 5,3,3,0
+#define OPENXCOM_VERSION_SHORT "Extended 5.3.4"
+#define OPENXCOM_VERSION_LONG "5.3.4.0"
+#define OPENXCOM_VERSION_NUMBER 5,3,4,0
 
 #ifndef OPENXCOM_VERSION_GIT
-#define OPENXCOM_VERSION_GIT " (v2019-04-13)"
+#define OPENXCOM_VERSION_GIT " (v2019-04-19)"
 #endif
