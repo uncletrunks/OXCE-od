c395a6013e3587d8e2a366c07450f44db8f7d425
Meridian <meridian@centrum.sk>
2019-04-22 12:42:54 +0200

    Updated version info, OXCE 5.4

--- src/version.h
+++ src/version.h
@@ -18,10 +18,10 @@
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define OPENXCOM_VERSION_SHORT "Extended 5.3.4"
-#define OPENXCOM_VERSION_LONG "5.3.4.0"
-#define OPENXCOM_VERSION_NUMBER 5,3,4,0
+#define OPENXCOM_VERSION_SHORT "Extended 5.4"
+#define OPENXCOM_VERSION_LONG "5.4.0.0"
+#define OPENXCOM_VERSION_NUMBER 5,4,0,0
 
 #ifndef OPENXCOM_VERSION_GIT
-#define OPENXCOM_VERSION_GIT " (v2019-04-19)"
+#define OPENXCOM_VERSION_GIT " (v2019-04-22)"
 #endif
