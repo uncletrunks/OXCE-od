46c17f7f1193e3093e6354db59976d36306bed32
Meridian <meridian@centrum.sk>
2019-09-03 14:10:47 +0200

    Not needed atm

--- src/Menu/OptionsModsState.cpp
+++ src/Menu/OptionsModsState.cpp
@@ -86,7 +86,7 @@ OptionsModsState::OptionsModsState() : _curMasterIdx(0)
 	_txtMaster->setText(tr("STR_BASE_GAME"));
 
 	// scan for masters
-	Options::refreshMods();
+	//Options::refreshMods(); // TODO: uncomment (and properly test!) after mod.io integration is merged
 	const std::map<std::string, ModInfo> &modInfos(Options::getModInfos());
 	std::vector<std::string> masterNames;
 	for (std::vector< std::pair<std::string, bool> >::const_iterator i = Options::mods.begin(); i != Options::mods.end(); ++i)
