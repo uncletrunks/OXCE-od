629a8f729ea3279992b61ec6349e7d2781e101f6
Meridian <meridian@centrum.sk>
2019-09-10 18:35:48 +0200

    Bump

--- src/version.h
+++ src/version.h
@@ -18,9 +18,9 @@
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define OPENXCOM_VERSION_SHORT "Extended 5.6.2"
-#define OPENXCOM_VERSION_LONG "5.6.2.0"
-#define OPENXCOM_VERSION_NUMBER 5,6,2,0
+#define OPENXCOM_VERSION_SHORT "Extended 5.6.3"
+#define OPENXCOM_VERSION_LONG "5.6.3.0"
+#define OPENXCOM_VERSION_NUMBER 5,6,3,0
 
 #ifndef OPENXCOM_VERSION_GIT
 #define OPENXCOM_VERSION_GIT " (v2019-09-10)"
