8821fcabf57bff5ad28ddd2f1ad9fcb53a1e331c
Meridian <meridian@centrum.sk>
2019-09-21 19:57:33 +0200

    Fix merge issue

--- src/Savegame/Tile.cpp
+++ src/Savegame/Tile.cpp
@@ -359,7 +359,7 @@ int Tile::openDoor(TilePart part, BattleUnit *unit, BattleActionType reserve, bo
 
 	if (_objects[part]->isDoor())
 	{
-		if (unit && unit->getArmor()->getSize() == 1) // don't allow double-wide units to open swinging doors due to engine limitations
+		if (unit && unit->getArmor()->getSize() > 1) // don't allow double-wide units to open swinging doors due to engine limitations
 			return -1;
 		if (unit && cost.Time && !cost.haveTU())
 			return 4;
