a0c79f9bdb94f96671391cd95d9d73ae6a81cedc
Yankes <Yankes@users.noreply.github.com>
2019-09-27 22:20:33 +0200

    Fix handing of offsets in script

--- src/Mod/Mod.cpp
+++ src/Mod/Mod.cpp
@@ -227,7 +227,7 @@ void Mod::resetGlobalStatics()
 class ModScriptGlobal : public ScriptGlobal
 {
 	size_t _modCurr = 0;
-	std::vector<std::string> _modNames;
+	std::vector<std::pair<std::string, int>> _modNames;
 
 	void loadRuleList(int &value, const YAML::Node &node) const
 	{
@@ -244,11 +244,11 @@ class ModScriptGlobal : public ScriptGlobal
 			}
 			else
 			{
-				for (size_t i = 0; i < _modNames.size(); ++i)
+				for (const auto& p : _modNames)
 				{
-					if (name == _modNames[i])
+					if (name == p.first)
 					{
-						value = (int)i;
+						value = p.second;
 						return;
 					}
 				}
@@ -258,9 +258,13 @@ class ModScriptGlobal : public ScriptGlobal
 	}
 	void saveRuleList(const int &value, YAML::Node &node) const
 	{
-		if ((size_t)value < _modNames.size())
+		for (const auto& p : _modNames)
 		{
-			node = _modNames[value];
+			if (value == p.second)
+			{
+				node = p.first;
+				return;
+			}
 		}
 	}
 
@@ -285,7 +289,7 @@ public:
 	{
 		auto name = "RuleList." + s;
 		addConst(name, (int)i);
-		_modNames.push_back(s);
+		_modNames.push_back(std::make_pair(s, i));
 	}
 	/// Set current mod id.
 	void setMod(int i)
@@ -1240,7 +1244,7 @@ void Mod::loadAll()
 		{
 			throwModOnErrorHelper(modId, "this mod name is already used");
 		}
-		_scriptGlobal->addMod(mods[i].first, (int)offset);
+		_scriptGlobal->addMod(mods[i].first, 1000 * (int)offset);
 		const ModInfo *modInfo = &Options::getModInfos().at(modId);
 		size_t size = modInfo->getReservedSpace();
 		_modData[i].name = modId;
@@ -1267,15 +1271,15 @@ void Mod::loadAll()
 	// vanilla resources load
 	_modCurrent = &_modData.at(0);
 	loadVanillaResources();
-	_surfaceOffsetBasebits = getSurfaceSet("BASEBITS.PCK")->getTotalFrames();
-	_surfaceOffsetBigobs = getRule("BIGOBS.PCK", "Sprite Set", _sets, true)->getTotalFrames(); // no lazy loading here yet
-	_surfaceOffsetFloorob = getSurfaceSet("FLOOROB.PCK")->getTotalFrames();
-	_surfaceOffsetHandob = getSurfaceSet("HANDOB.PCK")->getTotalFrames();
-	_surfaceOffsetHit = getSurfaceSet("HIT.PCK")->getTotalFrames();
-	_surfaceOffsetSmoke = getSurfaceSet("SMOKE.PCK")->getTotalFrames();
+	_surfaceOffsetBasebits = _sets["BASEBITS.PCK"]->getMaxSharedFrames();
+	_surfaceOffsetBigobs = _sets["BIGOBS.PCK"]->getMaxSharedFrames();
+	_surfaceOffsetFloorob = _sets["FLOOROB.PCK"]->getMaxSharedFrames();
+	_surfaceOffsetHandob = _sets["HANDOB.PCK"]->getMaxSharedFrames();
+	_surfaceOffsetHit = _sets["HIT.PCK"]->getMaxSharedFrames();
+	_surfaceOffsetSmoke = _sets["SMOKE.PCK"]->getMaxSharedFrames();
 
-	_soundOffsetBattle = getSoundSet("BATTLE.CAT")->getTotalSounds();
-	_soundOffsetGeo = getSoundSet("GEO.CAT")->getTotalSounds();
+	_soundOffsetBattle = _sounds["BATTLE.CAT"]->getMaxSharedSounds();
+	_soundOffsetGeo = _sounds["GEO.CAT"]->getMaxSharedSounds();
 
 	// load rest rulesets
 	for (size_t i = 0; mods.size() > i; ++i)
@@ -4828,7 +4832,7 @@ void offset(const Mod *m, int &base, int modId)
 	int baseMax = (m->*f);
 	if (base >= baseMax)
 	{
-		base += modId * 1000;
+		base += modId;
 	}
 }
 
