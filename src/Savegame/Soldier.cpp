f4b02f0fe5948779d0a3d9cd5d1f3622a2dcb82d
Meridian <meridian@centrum.sk>
2019-10-05 15:19:19 +0200

    Consider mana during min stat check too

--- src/Savegame/Soldier.cpp
+++ src/Savegame/Soldier.cpp
@@ -1195,6 +1195,7 @@ bool Soldier::isEligibleForTransformation(RuleSoldierTransformation *transformat
 		_currentStats.firing < minStats.firing ||
 		_currentStats.throwing < minStats.throwing ||
 		_currentStats.melee < minStats.melee ||
+		_currentStats.mana < minStats.mana ||
 		_currentStats.strength < minStats.strength ||
 		_currentStats.psiStrength < minStats.psiStrength ||
 		(_currentStats.psiSkill < minStats.psiSkill && minStats.psiSkill != 0)) // The != 0 is required for the "psi training at any time" option, as it sets skill to negative in training
