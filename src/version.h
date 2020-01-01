0be4901b6ad85c7c4c862bf9f1dc796e80e0f0bd
Meridian <meridian@centrum.sk>
2020-01-01 16:39:52 +0100

    OXCE v6.3.1

--- src/version.h
+++ src/version.h
@@ -18,10 +18,10 @@
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define OPENXCOM_VERSION_SHORT "Extended 6.3"
-#define OPENXCOM_VERSION_LONG "6.3.0.0"
-#define OPENXCOM_VERSION_NUMBER 6,3,0,0
+#define OPENXCOM_VERSION_SHORT "Extended 6.3.1"
+#define OPENXCOM_VERSION_LONG "6.3.1.0"
+#define OPENXCOM_VERSION_NUMBER 6,3,1,0
 
 #ifndef OPENXCOM_VERSION_GIT
-#define OPENXCOM_VERSION_GIT " (v2019-12-30)"
+#define OPENXCOM_VERSION_GIT " (v2020-01-01)"
 #endif
