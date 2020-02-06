56ed8339db3c3827eb805ec226fb10123ebd8e41
Meridian <meridian@centrum.sk>
2020-02-06 23:17:35 +0100

    Mission zone fix

--- src/Savegame/AlienMission.cpp
+++ src/Savegame/AlienMission.cpp
@@ -951,7 +951,7 @@ std::pair<double, double> AlienMission::getWaypoint(const MissionWave &wave, con
 
 	if (_missionSiteZone != -1 && wave.objective && trajectory.getZone(nextWaypoint) == (size_t)(_rule.getSpawnZone()))
 	{
-		const MissionArea *area = &region.getMissionZones().at(_rule.getObjective()).areas.at(_missionSiteZone);
+		const MissionArea *area = &region.getMissionZones().at(_rule.getSpawnZone()).areas.at(_missionSiteZone);
 		return std::make_pair(area->lonMin, area->latMin);
 	}
 
