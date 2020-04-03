20a521a13b2778ec3b4ef65085cb20275207c51a
Meridian <meridian@centrum.sk>
2020-04-03 19:25:37 +0200

    Version bump

--- src/version.h
+++ src/version.h
@@ -18,10 +18,10 @@
  * along with OpenXcom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define OPENXCOM_VERSION_SHORT "Extended 6.4.2"
-#define OPENXCOM_VERSION_LONG "6.4.2.0"
-#define OPENXCOM_VERSION_NUMBER 6,4,2,0
+#define OPENXCOM_VERSION_SHORT "Extended 6.4.3"
+#define OPENXCOM_VERSION_LONG "6.4.3.0"
+#define OPENXCOM_VERSION_NUMBER 6,4,3,0
 
 #ifndef OPENXCOM_VERSION_GIT
-#define OPENXCOM_VERSION_GIT " (v2020-03-28)"
+#define OPENXCOM_VERSION_GIT " (v2020-04-03)"
 #endif
