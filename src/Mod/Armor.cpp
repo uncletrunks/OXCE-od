Yankes/OpenXcom

Add new vector load helpers

src/Mod/Armor.cpp
@@ -113,10 +113,10 @@ void Armor::load(const YAML::Node &node, const ModScript &parsers, Mod *mod)
 	}
 	else if (node["corpseBattle"])
 	{
-		_corpseBattle = node["corpseBattle"].as< std::vector<std::string> >();
-		_corpseGeo = _corpseBattle[0];
+		mod->loadNames(_type, _corpseBattle, node["corpseBattle"]);
+		_corpseGeo = _corpseBattle.at(0);
 	}
-	_builtInWeapons = node["builtInWeapons"].as<std::vector<std::string> >(_builtInWeapons);
+	mod->loadNames(_type, _builtInWeapons, node["builtInWeapons"]);
 	_corpseGeo = node["corpseGeo"].as<std::string>(_corpseGeo);
 	_storeItem = node["storeItem"].as<std::string>(_storeItem);
 	_specWeapon = node["specialWeapon"].as<std::string>(_specWeapon);
@@ -166,7 +166,7 @@ void Armor::load(const YAML::Node &node, const ModScript &parsers, Mod *mod)
 			_damageModifier[i] = dmg[i].as<float>();
 		}
 	}
-	_loftempsSet = node["loftempsSet"].as< std::vector<int> >(_loftempsSet);
+	mod->loadInts(_type, _loftempsSet, node["loftempsSet"]);
 	if (node["loftemps"])
 		_loftempsSet.push_back(node["loftemps"].as<int>());
 	_deathFrames = node["deathFrames"].as<int>(_deathFrames);
@@ -226,14 +226,14 @@ void Armor::load(const YAML::Node &node, const ModScript &parsers, Mod *mod)
 	_hairColorGroup = node["spriteHairGroup"].as<int>(_hairColorGroup);
 	_rankColorGroup = node["spriteRankGroup"].as<int>(_rankColorGroup);
 	_utileColorGroup = node["spriteUtileGroup"].as<int>(_utileColorGroup);
-	_faceColor = node["spriteFaceColor"].as<std::vector<int> >(_faceColor);
-	_hairColor = node["spriteHairColor"].as<std::vector<int> >(_hairColor);
-	_rankColor = node["spriteRankColor"].as<std::vector<int> >(_rankColor);
-	_utileColor = node["spriteUtileColor"].as<std::vector<int> >(_utileColor);
+	mod->loadInts(_type, _faceColor, node["spriteFaceColor"]);
+	mod->loadInts(_type, _hairColor, node["spriteHairColor"]);
+	mod->loadInts(_type, _rankColor, node["spriteRankColor"]);
+	mod->loadInts(_type, _utileColor, node["spriteUtileColor"]);
 
 	_battleUnitScripts.load(_type, node, parsers.battleUnitScripts);
 
-	_units = node["units"].as< std::vector<std::string> >(_units);
+	mod->loadUnorederedNames(_type, _units, node["units"]);
 	_scriptValues.load(node, parsers.getShared());
 	mod->loadSpriteOffset(_type, _customArmorPreviewIndex, node["customArmorPreviewIndex"], "CustomArmorPreviews");
 	loadTriBoolHelper(_allowsRunning, node["allowsRunning"]);

src/Mod/RuleBaseFacility.cpp
@@ -63,7 +63,7 @@ void RuleBaseFacility::load(const YAML::Node &node, Mod *mod, int listOrder)
 		load(parent, mod, listOrder);
 	}
 	_type = node["type"].as<std::string>(_type);
-	_requires = node["requires"].as< std::vector<std::string> >(_requires);
+	mod->loadUnorederedNames(_type, _requires, node["requires"]);
 
 	mod->loadBaseFunction(_type, _requiresBaseFunc, node["requiresBaseFunc"]);
 	mod->loadBaseFunction(_type, _provideBaseFunc, node["provideBaseFunc"]);
@@ -147,10 +147,10 @@ void RuleBaseFacility::load(const YAML::Node &node, Mod *mod, int listOrder)
 		}
 	}
 
-	_leavesBehindOnSellNames = node["leavesBehindOnSell"].as< std::vector<std::string> >(_leavesBehindOnSellNames);
+	mod->loadNames(_type, _leavesBehindOnSellNames, node["leavesBehindOnSell"]);
 	_removalTime = node["removalTime"].as<int>(_removalTime);
 	_canBeBuiltOver = node["canBeBuiltOver"].as<bool>(_canBeBuiltOver);
-	_buildOverFacilitiesNames = node["buildOverFacilities"].as< std::vector<std::string> >(_buildOverFacilitiesNames);
+	mod->loadUnorederedNames(_type, _buildOverFacilitiesNames, node["buildOverFacilities"]);
 	std::sort(_buildOverFacilities.begin(), _buildOverFacilities.end());
 
 	_storageTiles = node["storageTiles"].as<std::vector<Position> >(_storageTiles);

src/Mod/RuleResearch.cpp
@@ -46,11 +46,11 @@ void RuleResearch::load(const YAML::Node &node, Mod* mod, int listOrder)
 	_spawnedItem = node["spawnedItem"].as<std::string>(_spawnedItem);
 	_cost = node["cost"].as<int>(_cost);
 	_points = node["points"].as<int>(_points);
-	_dependenciesName = node["dependencies"].as< std::vector<std::string> >(_dependenciesName);
-	_unlocksName = node["unlocks"].as< std::vector<std::string> >(_unlocksName);
-	_disablesName = node["disables"].as< std::vector<std::string> >(_disablesName);
-	_getOneFreeName = node["getOneFree"].as< std::vector<std::string> >(_getOneFreeName);
-	_requiresName = node["requires"].as< std::vector<std::string> >(_requiresName);
+	mod->loadUnorederedNames(_name, _dependenciesName, node["dependencies"]);
+	mod->loadUnorederedNames(_name, _unlocksName, node["unlocks"]);
+	mod->loadUnorederedNames(_name, _disablesName, node["disables"]);
+	mod->loadUnorederedNames(_name, _getOneFreeName, node["getOneFree"]);
+	mod->loadUnorederedNames(_name, _requiresName, node["requires"]);
 	mod->loadBaseFunction(_name, _requiresBaseFunc, node["requiresBaseFunc"]);
 	_sequentialGetOneFree = node["sequentialGetOneFree"].as<bool>(_sequentialGetOneFree);
 	_getOneFreeProtectedName = node["getOneFreeProtected"].as< std::map<std::string, std::vector<std::string> > >(_getOneFreeProtectedName);

src/Mod/RuleSkill.cpp
@@ -34,11 +34,11 @@ RuleSkill::RuleSkill(const std::string& type) : _type(type),
  * @param node YAML node.
  * @param mod Mod for the skill.
  */
-void RuleSkill::load(const YAML::Node& node, const ModScript& parsers)
+void RuleSkill::load(const YAML::Node& node, Mod *mod, const ModScript& parsers)
 {
 	if (const YAML::Node& parent = node["refNode"])
 	{
-		load(parent, parsers);
+		load(parent, mod, parsers);
 	}
 	_type = node["type"].as<std::string>(_type);
 
@@ -58,8 +58,8 @@ void RuleSkill::load(const YAML::Node& node, const ModScript& parsers)
 	_cost.loadCost(node, "Use");
 	_flat.loadPercent(node, "Use");
 
-	_compatibleWeaponNames = node["compatibleWeapons"].as<std::vector<std::string> >(_compatibleWeaponNames);
-	_requiredBonusNames = node["requiredBonuses"].as<std::vector<std::string> >(_requiredBonusNames);
+	mod->loadUnorederedNames(_type, _compatibleWeaponNames, node["compatibleWeapons"]);
+	mod->loadUnorederedNames(_type,_requiredBonusNames, node["requiredBonuses"]);
 
 	_scriptValues.load(node, parsers.getShared());
 

src/Mod/RuleSoldier.cpp
@@ -79,7 +79,7 @@ void RuleSoldier::load(const YAML::Node &node, Mod *mod, int listOrder, const Mo
 		_type = "STR_SOLDIER";
 
 	//requires
-	_requires = node["requires"].as< std::vector<std::string> >(_requires);
+	mod->loadUnorederedNames(_type, _requires, node["requires"]);
 	mod->loadBaseFunction(_type, _requiresBuyBaseFunc, node["requiresBuyBaseFunc"]);
 
 
@@ -174,13 +174,13 @@ void RuleSoldier::load(const YAML::Node &node, Mod *mod, int listOrder, const Mo
 		_statStrings.push_back(statString);
 	}
 
-	_rankStrings = node["rankStrings"].as< std::vector<std::string> >(_rankStrings);
+	mod->loadNames(_type, _rankStrings, node["rankStrings"]);
 	mod->loadSpriteOffset(_type, _rankSprite, node["rankSprite"], "BASEBITS.PCK");
 	mod->loadSpriteOffset(_type, _rankSpriteBattlescape, node["rankBattleSprite"], "SMOKE.PCK");
 	mod->loadSpriteOffset(_type, _rankSpriteTiny, node["rankTinySprite"], "TinyRanks");
 	mod->loadSpriteOffset(_type, _skillIconSprite, node["skillIconSprite"], "SPICONS.DAT");
 
-	_skillNames = node["skills"].as<std::vector<std::string> >(_skillNames);
+	mod->loadNames(_type, _skillNames, node["skills"]);
 
 	_listOrder = node["listOrder"].as<int>(_listOrder);
 	if (!_listOrder)

src/Mod/RuleSoldierTransformation.cpp
@@ -54,7 +54,7 @@ void RuleSoldierTransformation::load(const YAML::Node &node, Mod* mod, int listO
 		_listOrder = listOrder;
 	}
 
-	_requires = node["requires"].as<std::vector<std::string > >(_requires);
+	mod->loadUnorederedNames(_name, _requires, node["requires"]);
 	mod->loadBaseFunction(_name, _requiresBaseFunc, node["requiresBaseFunc"]);
 	_producedItem = node["producedItem"].as<std::string >(_producedItem);
 	_producedSoldierType = node["producedSoldierType"].as<std::string >(_producedSoldierType);
@@ -65,9 +65,9 @@ void RuleSoldierTransformation::load(const YAML::Node &node, Mod* mod, int listO
 	_allowsDeadSoldiers = node["allowsDeadSoldiers"].as<bool >(_allowsDeadSoldiers);
 	_allowsLiveSoldiers = node["allowsLiveSoldiers"].as<bool >(_allowsLiveSoldiers);
 	_allowsWoundedSoldiers = node["allowsWoundedSoldiers"].as<bool >(_allowsWoundedSoldiers);
-	_allowedSoldierTypes = node["allowedSoldierTypes"].as<std::vector<std::string > >(_allowedSoldierTypes);
-	_requiredPreviousTransformations = node["requiredPreviousTransformations"].as<std::vector<std::string > >(_requiredPreviousTransformations);
-	_forbiddenPreviousTransformations = node["forbiddenPreviousTransformations"].as<std::vector<std::string > >(_forbiddenPreviousTransformations);
+	mod->loadUnorederedNames(_name, _allowedSoldierTypes, node["allowedSoldierTypes"]);
+	mod->loadUnorederedNames(_name, _requiredPreviousTransformations, node["requiredPreviousTransformations"]);
+	mod->loadUnorederedNames(_name, _forbiddenPreviousTransformations, node["forbiddenPreviousTransformations"]);
 	_requiredMinStats = node["requiredMinStats"].as<UnitStats >(_requiredMinStats);
 	_requiredItems = node["requiredItems"].as< std::map<std::string, int> >(_requiredItems);
 	_requiredCommendations = node["requiredCommendations"].as< std::map<std::string, int> >(_requiredCommendations);

src/Mod/RuleStartingCondition.cpp
@@ -47,26 +47,26 @@ RuleStartingCondition::~RuleStartingCondition()
  * Loads the Starting Conditions from a YAML file.
  * @param node YAML node.
  */
-void RuleStartingCondition::load(const YAML::Node& node)
+void RuleStartingCondition::load(const YAML::Node& node, Mod *mod)
 {
 	if (const YAML::Node& parent = node["refNode"])
 	{
-		load(parent);
+		load(parent, mod);
 	}
 	_type = node["type"].as<std::string>(_type);
 	_defaultArmor = node["defaultArmor"].as< std::map<std::string, std::map<std::string, int> > >(_defaultArmor);
-	_allowedArmors = node["allowedArmors"].as< std::vector<std::string> >(_allowedArmors);
-	_forbiddenArmors = node["forbiddenArmors"].as< std::vector<std::string> >(_forbiddenArmors);
-	_allowedVehicles = node["allowedVehicles"].as< std::vector<std::string> >(_allowedVehicles);
-	_forbiddenVehicles = node["forbiddenVehicles"].as< std::vector<std::string> >(_forbiddenVehicles);
-	_allowedItems = node["allowedItems"].as< std::vector<std::string> >(_allowedItems);
-	_forbiddenItems = node["forbiddenItems"].as< std::vector<std::string> >(_forbiddenItems);
-	_allowedItemCategories = node["allowedItemCategories"].as< std::vector<std::string> >(_allowedItemCategories);
-	_forbiddenItemCategories = node["forbiddenItemCategories"].as< std::vector<std::string> >(_forbiddenItemCategories);
-	_allowedCraft = node["allowedCraft"].as< std::vector<std::string> >(_allowedCraft);
-	_forbiddenCraft = node["forbiddenCraft"].as< std::vector<std::string> >(_forbiddenCraft);
-	_allowedSoldierTypes = node["allowedSoldierTypes"].as< std::vector<std::string> >(_allowedSoldierTypes);
-	_forbiddenSoldierTypes = node["forbiddenSoldierTypes"].as< std::vector<std::string> >(_forbiddenSoldierTypes);
+	mod->loadUnorederedNames(_type, _allowedArmors, node["allowedArmors"]);
+	mod->loadUnorederedNames(_type, _forbiddenArmors, node["forbiddenArmors"]);
+	mod->loadUnorederedNames(_type, _allowedVehicles, node["allowedVehicles"]);
+	mod->loadUnorederedNames(_type, _forbiddenVehicles, node["forbiddenVehicles"]);
+	mod->loadUnorederedNames(_type, _allowedItems, node["allowedItems"]);
+	mod->loadUnorederedNames(_type, _forbiddenItems, node["forbiddenItems"]);
+	mod->loadUnorederedNames(_type, _allowedItemCategories, node["allowedItemCategories"]);
+	mod->loadUnorederedNames(_type, _forbiddenItemCategories, node["forbiddenItemCategories"]);
+	mod->loadUnorederedNames(_type, _allowedCraft, node["allowedCraft"]);
+	mod->loadUnorederedNames(_type, _forbiddenCraft, node["forbiddenCraft"]);
+	mod->loadUnorederedNames(_type, _allowedSoldierTypes, node["allowedSoldierTypes"]);
+	mod->loadUnorederedNames(_type, _forbiddenSoldierTypes, node["forbiddenSoldierTypes"]);
 	_requiredItems = node["requiredItems"].as< std::map<std::string, int> >(_requiredItems);
 	_destroyRequiredItems = node["destroyRequiredItems"].as<bool>(_destroyRequiredItems);
 

uncletrunks/OXCE-od

src/Mod/Armor.cpp
@@ -113,10 +113,10 @@ void Armor::load(const YAML::Node &node, const ModScript &parsers, Mod *mod)
 	}
 	else if (node["corpseBattle"])
 	{
-		_corpseBattle = node["corpseBattle"].as< std::vector<std::string> >();
-		_corpseGeo = _corpseBattle[0];
+		mod->loadNames(_type, _corpseBattle, node["corpseBattle"]);
+		_corpseGeo = _corpseBattle.at(0);
 	}
-	_builtInWeapons = node["builtInWeapons"].as<std::vector<std::string> >(_builtInWeapons);
+	mod->loadNames(_type, _builtInWeapons, node["builtInWeapons"]);
 	_corpseGeo = node["corpseGeo"].as<std::string>(_corpseGeo);
 	_storeItem = node["storeItem"].as<std::string>(_storeItem);
 	_specWeapon = node["specialWeapon"].as<std::string>(_specWeapon);
@@ -166,7 +166,7 @@ void Armor::load(const YAML::Node &node, const ModScript &parsers, Mod *mod)
 			_damageModifier[i] = dmg[i].as<float>();
 		}
 	}
-	_loftempsSet = node["loftempsSet"].as< std::vector<int> >(_loftempsSet);
+	mod->loadInts(_type, _loftempsSet, node["loftempsSet"]);
 	if (node["loftemps"])
 		_loftempsSet.push_back(node["loftemps"].as<int>());
 	_deathFrames = node["deathFrames"].as<int>(_deathFrames);
@@ -226,14 +226,14 @@ void Armor::load(const YAML::Node &node, const ModScript &parsers, Mod *mod)
 	_hairColorGroup = node["spriteHairGroup"].as<int>(_hairColorGroup);
 	_rankColorGroup = node["spriteRankGroup"].as<int>(_rankColorGroup);
 	_utileColorGroup = node["spriteUtileGroup"].as<int>(_utileColorGroup);
-	_faceColor = node["spriteFaceColor"].as<std::vector<int> >(_faceColor);
-	_hairColor = node["spriteHairColor"].as<std::vector<int> >(_hairColor);
-	_rankColor = node["spriteRankColor"].as<std::vector<int> >(_rankColor);
-	_utileColor = node["spriteUtileColor"].as<std::vector<int> >(_utileColor);
+	mod->loadInts(_type, _faceColor, node["spriteFaceColor"]);
+	mod->loadInts(_type, _hairColor, node["spriteHairColor"]);
+	mod->loadInts(_type, _rankColor, node["spriteRankColor"]);
+	mod->loadInts(_type, _utileColor, node["spriteUtileColor"]);
 
 	_battleUnitScripts.load(_type, node, parsers.battleUnitScripts);
 
-	_units = node["units"].as< std::vector<std::string> >(_units);
+	mod->loadUnorderedNames(_type, _units, node["units"]);
 	_scriptValues.load(node, parsers.getShared());
 	mod->loadSpriteOffset(_type, _customArmorPreviewIndex, node["customArmorPreviewIndex"], "CustomArmorPreviews");
 	loadTriBoolHelper(_allowsRunning, node["allowsRunning"]);

src/Mod/RuleBaseFacility.cpp
@@ -63,7 +63,7 @@ void RuleBaseFacility::load(const YAML::Node &node, Mod *mod, int listOrder)
 		load(parent, mod, listOrder);
 	}
 	_type = node["type"].as<std::string>(_type);
-	_requires = node["requires"].as< std::vector<std::string> >(_requires);
+	mod->loadUnorderedNames(_type, _requires, node["requires"]);
 
 	mod->loadBaseFunction(_type, _requiresBaseFunc, node["requiresBaseFunc"]);
 	mod->loadBaseFunction(_type, _provideBaseFunc, node["provideBaseFunc"]);
@@ -147,10 +147,10 @@ void RuleBaseFacility::load(const YAML::Node &node, Mod *mod, int listOrder)
 		}
 	}
 
-	_leavesBehindOnSellNames = node["leavesBehindOnSell"].as< std::vector<std::string> >(_leavesBehindOnSellNames);
+	mod->loadNames(_type, _leavesBehindOnSellNames, node["leavesBehindOnSell"]);
 	_removalTime = node["removalTime"].as<int>(_removalTime);
 	_canBeBuiltOver = node["canBeBuiltOver"].as<bool>(_canBeBuiltOver);
-	_buildOverFacilitiesNames = node["buildOverFacilities"].as< std::vector<std::string> >(_buildOverFacilitiesNames);
+	mod->loadUnorderedNames(_type, _buildOverFacilitiesNames, node["buildOverFacilities"]);
 	std::sort(_buildOverFacilities.begin(), _buildOverFacilities.end());
 
 	_storageTiles = node["storageTiles"].as<std::vector<Position> >(_storageTiles);

src/Mod/RuleResearch.cpp
@@ -46,11 +46,11 @@ void RuleResearch::load(const YAML::Node &node, Mod* mod, int listOrder)
 	_spawnedItem = node["spawnedItem"].as<std::string>(_spawnedItem);
 	_cost = node["cost"].as<int>(_cost);
 	_points = node["points"].as<int>(_points);
-	_dependenciesName = node["dependencies"].as< std::vector<std::string> >(_dependenciesName);
-	_unlocksName = node["unlocks"].as< std::vector<std::string> >(_unlocksName);
-	_disablesName = node["disables"].as< std::vector<std::string> >(_disablesName);
-	_getOneFreeName = node["getOneFree"].as< std::vector<std::string> >(_getOneFreeName);
-	_requiresName = node["requires"].as< std::vector<std::string> >(_requiresName);
+	mod->loadUnorderedNames(_name, _dependenciesName, node["dependencies"]);
+	mod->loadUnorderedNames(_name, _unlocksName, node["unlocks"]);
+	mod->loadUnorderedNames(_name, _disablesName, node["disables"]);
+	mod->loadUnorderedNames(_name, _getOneFreeName, node["getOneFree"]);
+	mod->loadUnorderedNames(_name, _requiresName, node["requires"]);
 	mod->loadBaseFunction(_name, _requiresBaseFunc, node["requiresBaseFunc"]);
 	_sequentialGetOneFree = node["sequentialGetOneFree"].as<bool>(_sequentialGetOneFree);
 	_getOneFreeProtectedName = node["getOneFreeProtected"].as< std::map<std::string, std::vector<std::string> > >(_getOneFreeProtectedName);

src/Mod/RuleSkill.cpp
@@ -34,11 +34,11 @@ RuleSkill::RuleSkill(const std::string& type) : _type(type),
  * @param node YAML node.
  * @param mod Mod for the skill.
  */
-void RuleSkill::load(const YAML::Node& node, const ModScript& parsers)
+void RuleSkill::load(const YAML::Node& node, Mod *mod, const ModScript& parsers)
 {
 	if (const YAML::Node& parent = node["refNode"])
 	{
-		load(parent, parsers);
+		load(parent, mod, parsers);
 	}
 	_type = node["type"].as<std::string>(_type);
 
@@ -58,8 +58,8 @@ void RuleSkill::load(const YAML::Node& node, const ModScript& parsers)
 	_cost.loadCost(node, "Use");
 	_flat.loadPercent(node, "Use");
 
-	_compatibleWeaponNames = node["compatibleWeapons"].as<std::vector<std::string> >(_compatibleWeaponNames);
-	_requiredBonusNames = node["requiredBonuses"].as<std::vector<std::string> >(_requiredBonusNames);
+	mod->loadUnorderedNames(_type, _compatibleWeaponNames, node["compatibleWeapons"]);
+	mod->loadUnorderedNames(_type,_requiredBonusNames, node["requiredBonuses"]);
 
 	_scriptValues.load(node, parsers.getShared());
 

src/Mod/RuleSoldier.cpp
@@ -79,7 +79,7 @@ void RuleSoldier::load(const YAML::Node &node, Mod *mod, int listOrder, const Mo
 		_type = "STR_SOLDIER";
 
 	//requires
-	_requires = node["requires"].as< std::vector<std::string> >(_requires);
+	mod->loadUnorderedNames(_type, _requires, node["requires"]);
 	mod->loadBaseFunction(_type, _requiresBuyBaseFunc, node["requiresBuyBaseFunc"]);
 
 
@@ -174,13 +174,13 @@ void RuleSoldier::load(const YAML::Node &node, Mod *mod, int listOrder, const Mo
 		_statStrings.push_back(statString);
 	}
 
-	_rankStrings = node["rankStrings"].as< std::vector<std::string> >(_rankStrings);
+	mod->loadNames(_type, _rankStrings, node["rankStrings"]);
 	mod->loadSpriteOffset(_type, _rankSprite, node["rankSprite"], "BASEBITS.PCK");
 	mod->loadSpriteOffset(_type, _rankSpriteBattlescape, node["rankBattleSprite"], "SMOKE.PCK");
 	mod->loadSpriteOffset(_type, _rankSpriteTiny, node["rankTinySprite"], "TinyRanks");
 	mod->loadSpriteOffset(_type, _skillIconSprite, node["skillIconSprite"], "SPICONS.DAT");
 
-	_skillNames = node["skills"].as<std::vector<std::string> >(_skillNames);
+	mod->loadNames(_type, _skillNames, node["skills"]);
 
 	_listOrder = node["listOrder"].as<int>(_listOrder);
 	if (!_listOrder)

src/Mod/RuleSoldierTransformation.cpp
@@ -54,7 +54,7 @@ void RuleSoldierTransformation::load(const YAML::Node &node, Mod* mod, int listO
 		_listOrder = listOrder;
 	}
 
-	_requires = node["requires"].as<std::vector<std::string > >(_requires);
+	mod->loadUnorderedNames(_name, _requires, node["requires"]);
 	mod->loadBaseFunction(_name, _requiresBaseFunc, node["requiresBaseFunc"]);
 	_producedItem = node["producedItem"].as<std::string >(_producedItem);
 	_producedSoldierType = node["producedSoldierType"].as<std::string >(_producedSoldierType);
@@ -65,9 +65,9 @@ void RuleSoldierTransformation::load(const YAML::Node &node, Mod* mod, int listO
 	_allowsDeadSoldiers = node["allowsDeadSoldiers"].as<bool >(_allowsDeadSoldiers);
 	_allowsLiveSoldiers = node["allowsLiveSoldiers"].as<bool >(_allowsLiveSoldiers);
 	_allowsWoundedSoldiers = node["allowsWoundedSoldiers"].as<bool >(_allowsWoundedSoldiers);
-	_allowedSoldierTypes = node["allowedSoldierTypes"].as<std::vector<std::string > >(_allowedSoldierTypes);
-	_requiredPreviousTransformations = node["requiredPreviousTransformations"].as<std::vector<std::string > >(_requiredPreviousTransformations);
-	_forbiddenPreviousTransformations = node["forbiddenPreviousTransformations"].as<std::vector<std::string > >(_forbiddenPreviousTransformations);
+	mod->loadUnorderedNames(_name, _allowedSoldierTypes, node["allowedSoldierTypes"]);
+	mod->loadUnorderedNames(_name, _requiredPreviousTransformations, node["requiredPreviousTransformations"]);
+	mod->loadUnorderedNames(_name, _forbiddenPreviousTransformations, node["forbiddenPreviousTransformations"]);
 	_includeBonusesForMinStats = node["includeBonusesForMinStats"].as<bool >(_includeBonusesForMinStats);
 	_requiredMinStats = node["requiredMinStats"].as<UnitStats >(_requiredMinStats);
 	_requiredItems = node["requiredItems"].as< std::map<std::string, int> >(_requiredItems);

src/Mod/RuleStartingCondition.cpp
@@ -47,26 +47,26 @@ RuleStartingCondition::~RuleStartingCondition()
  * Loads the Starting Conditions from a YAML file.
  * @param node YAML node.
  */
-void RuleStartingCondition::load(const YAML::Node& node)
+void RuleStartingCondition::load(const YAML::Node& node, Mod *mod)
 {
 	if (const YAML::Node& parent = node["refNode"])
 	{
-		load(parent);
+		load(parent, mod);
 	}
 	_type = node["type"].as<std::string>(_type);
 	_defaultArmor = node["defaultArmor"].as< std::map<std::string, std::map<std::string, int> > >(_defaultArmor);
-	_allowedArmors = node["allowedArmors"].as< std::vector<std::string> >(_allowedArmors);
-	_forbiddenArmors = node["forbiddenArmors"].as< std::vector<std::string> >(_forbiddenArmors);
-	_allowedVehicles = node["allowedVehicles"].as< std::vector<std::string> >(_allowedVehicles);
-	_forbiddenVehicles = node["forbiddenVehicles"].as< std::vector<std::string> >(_forbiddenVehicles);
-	_allowedItems = node["allowedItems"].as< std::vector<std::string> >(_allowedItems);
-	_forbiddenItems = node["forbiddenItems"].as< std::vector<std::string> >(_forbiddenItems);
-	_allowedItemCategories = node["allowedItemCategories"].as< std::vector<std::string> >(_allowedItemCategories);
-	_forbiddenItemCategories = node["forbiddenItemCategories"].as< std::vector<std::string> >(_forbiddenItemCategories);
-	_allowedCraft = node["allowedCraft"].as< std::vector<std::string> >(_allowedCraft);
-	_forbiddenCraft = node["forbiddenCraft"].as< std::vector<std::string> >(_forbiddenCraft);
-	_allowedSoldierTypes = node["allowedSoldierTypes"].as< std::vector<std::string> >(_allowedSoldierTypes);
-	_forbiddenSoldierTypes = node["forbiddenSoldierTypes"].as< std::vector<std::string> >(_forbiddenSoldierTypes);
+	mod->loadUnorderedNames(_type, _allowedArmors, node["allowedArmors"]);
+	mod->loadUnorderedNames(_type, _forbiddenArmors, node["forbiddenArmors"]);
+	mod->loadUnorderedNames(_type, _allowedVehicles, node["allowedVehicles"]);
+	mod->loadUnorderedNames(_type, _forbiddenVehicles, node["forbiddenVehicles"]);
+	mod->loadUnorderedNames(_type, _allowedItems, node["allowedItems"]);
+	mod->loadUnorderedNames(_type, _forbiddenItems, node["forbiddenItems"]);
+	mod->loadUnorderedNames(_type, _allowedItemCategories, node["allowedItemCategories"]);
+	mod->loadUnorderedNames(_type, _forbiddenItemCategories, node["forbiddenItemCategories"]);
+	mod->loadUnorderedNames(_type, _allowedCraft, node["allowedCraft"]);
+	mod->loadUnorderedNames(_type, _forbiddenCraft, node["forbiddenCraft"]);
+	mod->loadUnorderedNames(_type, _allowedSoldierTypes, node["allowedSoldierTypes"]);
+	mod->loadUnorderedNames(_type, _forbiddenSoldierTypes, node["forbiddenSoldierTypes"]);
 	_requiredItems = node["requiredItems"].as< std::map<std::string, int> >(_requiredItems);
 	_destroyRequiredItems = node["destroyRequiredItems"].as<bool>(_destroyRequiredItems);
 