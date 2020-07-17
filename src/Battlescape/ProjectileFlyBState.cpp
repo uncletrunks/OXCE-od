aa044dabc5c28be3624c4376435ed9d46fa725e8
Meridian <meridian@centrum.sk>
2020-07-17 21:24:02 +0200

    Shooting at yourself doesn't count

--- src/Battlescape/ProjectileFlyBState.cpp
+++ src/Battlescape/ProjectileFlyBState.cpp
@@ -684,9 +684,10 @@ void ProjectileFlyBState::think()
 			}
 			else
 			{
-				if (_parent->getSave()->getTile(_action.target)->getUnit())
+				auto tmpUnit = _parent->getSave()->getTile(_action.target)->getUnit();
+				if (tmpUnit && tmpUnit != _unit)
 				{
-					_parent->getSave()->getTile(_action.target)->getUnit()->getStatistics()->shotAtCounter++; // Only counts for guns, not throws or launches
+					tmpUnit->getStatistics()->shotAtCounter++; // Only counts for guns, not throws or launches
 				}
 
 				_parent->getMap()->resetCameraSmoothing();
