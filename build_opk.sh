ed8e79491012797cad5bed9b1fdb6af61a8b044a
uncletrunks <uncletrunks@protonmail.com>
2020-11-18 02:34:49 -0600

    fixes

--- build_opk.sh
+++ build_opk.sh
@@ -7,4 +7,4 @@ convert res/linux/icons/openxcom_48x48.png  -resize 32x32! bin/icon.png
 mksquashfs \
     bin/*         \
     bin/icon.png                      \
-    default.gcw0.desktop openxcom-$(date '+%s').opk -noappend
+    default.gcw0.desktop oxce-$(date '+%s').opk -noappend
